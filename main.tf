f25a420e5c2bfa0876791788cecb67dbe7c54aad
heder redon <hederredon@gmail.com>
2024-02-02 12:41:10 -0500

    changed iam policy path

--- main.tf
+++ main.tf
@@ -431,34 +431,34 @@ resource "aws_wafv2_web_acl_logging_configuration" "example" {
 }
 
 resource "aws_cloudwatch_log_resource_policy" "knote-waf-policy" {
-#   policy_document = data.aws_iam_policy_document.knote-waf-policy.json
-  policy_document = file("./iam_policy.json")
+  policy_document = data.aws_iam_policy_document.knote-waf-policy.json
+#   policy_document = file("./iam_policy.json")
   policy_name     = "webacl-policy-knote-waf"
 }
 
-# data "aws_iam_policy_document" "knote-waf-policy" {
-#   version = "2012-10-17"
-#   statement {
-#     effect = "Allow"
-#     principals {
-#       identifiers = ["delivery.logs.amazonaws.com"]
-#       type        = "Service"
-#     }
-#     actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
-#     resources = ["${aws_cloudwatch_log_group.example.arn}:*"]
-#     condition {
-#       test     = "ArnLike"
-#       values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
-#       variable = "aws:SourceArn"
-#     }
-#     condition {
-#       test     = "StringEquals"
-#       values   = [tostring(data.aws_caller_identity.current.account_id)]
-#       variable = "aws:SourceAccount"
-#     }
-#   }
-# }
+data "aws_iam_policy_document" "knote-waf-policy" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+    actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["${aws_cloudwatch_log_group.example.arn}:*"]
+    condition {
+      test     = "ArnLike"
+      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
+      variable = "aws:SourceArn"
+    }
+    condition {
+      test     = "StringEquals"
+      values   = [tostring(data.aws_caller_identity.current.account_id)]
+      variable = "aws:SourceAccount"
+    }
+  }
+}
 
-# data "aws_region" "current" {}
+data "aws_region" "current" {}
 
-# data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {}
